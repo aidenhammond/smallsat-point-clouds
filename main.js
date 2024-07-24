2f2a0d46fae32208a496fbdd438af819a6ff96a1
Aiden <32718251+aidenhammond@users.noreply.github.com>
2024-07-24 14:35:28 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+import * as THREE from './modules/three.module.js';
+import { PCDLoader } from './modules/loaders/PCDLoader.js';
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scene = document.querySelector("a-scene");
+  const entity = document.getElementById("point-cloud");
+
+  scene.addEventListener("loaded", () => {
+    document.querySelector(".arjs-loader").style.display = "none";
+  });
+
+  const loader = new PCDLoader();
+  loader.load('https://aidenhammond.github.io/smallsat-point-clouds/exp-150.pcd', function (points) {
+    points.geometry.center();
+    points.scale.set(0.01, 0.01, 0.01); // Adjust scale as needed
+    entity.object3D.add(points);
+  });
+
+  function animate() {
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
